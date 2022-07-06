412fc160109f359e169287ecceff88429d29fe29
asus <makif_cetin@hotmail.com>
2022-07-06 22:44:48 +0300

    yeni proje

--- src/deneme1.java
+++ src/deneme1.java
@@ -1,5 +1,11 @@
 public class deneme1 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("1");
+
+        System.out.println("yeni proje");
+
     }
+
+
 }
